7b5aadadbc9f7f2f206ae5b35d24e00ea4415776
nafees nazik <nafeesnazik10@gmail.com>
2022-03-16 12:05:06 +0530

    feat: add styled function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-export const foo = 'foo'
+import * as React from "react"
+import { cva, cx } from "class-variance-authority"
+import type {
+  VariantProps,
+  ClassValue,
+  ClassProp,
+  VariantsSchema,
+  VariantsConfig,
+} from "class-variance-authority"
+
+export type IntrinsicElementsKeys = keyof JSX.IntrinsicElements
+
+export function styled<T extends IntrinsicElementsKeys>(
+  Tag: T,
+  forwardRef?: boolean,
+) {
+  return function wrapper<Variants extends VariantsSchema>(
+    base?: ClassValue,
+    config?:
+      | (Variants extends VariantsSchema
+          ? {
+              variants?: Variants | undefined
+              defaultVariants?: VariantsConfig<Variants> | undefined
+              compoundVariants?:
+                | (Variants extends VariantsSchema
+                    ? VariantsConfig<Variants> & ClassProp
+                    : ClassProp)[]
+                | undefined
+            }
+          : never)
+      | undefined,
+  ) {
+    const classes = cva(base, config)
+
+    function StyledWrapper(
+      props: JSX.IntrinsicElements[typeof Tag] &
+        Omit<VariantProps<typeof classes>, "class">,
+      ref: any,
+    ) {
+      // Grab a shallow copy of the props
+      let _props = Object.assign({}, props)
+      props.className = cx(classes(), props.className)
+      _props.ref = ref
+
+      let _as = Tag
+
+      return React.createElement(_as, _props)
+    }
+
+    return forwardRef ? React.forwardRef(StyledWrapper) : StyledWrapper
+  }
+}
